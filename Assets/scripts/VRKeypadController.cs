6ae432c8a45e4bec36f4dd376512d79ae709d050
juligric <49377944@est.ort.edu.ar>
2025-08-25 10:25:49 -0300

    ya asigne los bototnes pero no se ven

--- Assets/scripts/VRKeypadController.cs
+++ Assets/scripts/VRKeypadController.cs
@@ -1,18 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
-public class NewBehaviourScript : MonoBehaviour
+public class VRKeypadController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text displayText;               // Texto para mostrar la contraseña ingresada
+    public string correctPassword = "1234";    // Contraseña correcta
+    private string currentInput = "";          // Contraseña ingresada hasta el momento
+
+    public GameObject box;                      // Objeto que se ocultará si la contraseña es correcta
+
+    // Se llama cuando se presiona un botón numérico
+    public void PressKey(string number)
     {
-        
+        Debug.Log("Tecla presionada: " + number);
+        currentInput += number;
+        UpdateDisplay();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Se llama para borrar el último carácter ingresado
+    public void DeleteLast()
     {
-        
+        if (currentInput.Length > 0)
+        {
+            currentInput = currentInput.Substring(0, currentInput.Length - 1);
+            Debug.Log("Borrado último carácter");
+            UpdateDisplay();
+        }
+        else
+        {
+            Debug.Log("Nada para borrar");
+        }
+    }
+
+    // Se llama cuando se presiona el botón Enter
+    public void Submit()
+    {
+        Debug.Log("Intento de envío: " + currentInput);
+        if (currentInput == correctPassword)
+        {
+            Debug.Log("Contraseña correcta");
+            OpenBox();
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+            currentInput = "";
+            UpdateDisplay();
+        }
+    }
+
+    // Actualiza el texto mostrado en pantalla
+    private void UpdateDisplay()
+    {
+        displayText.text = currentInput;
+    }
+
+    // Acción para abrir/ocultar la caja
+    private void OpenBox()
+    {
+        if (box != null)
+        {
+            box.SetActive(false); // Desactiva la caja
+            Debug.Log("Caja abierta (oculta)");
+        }
+        else
+        {
+            Debug.LogWarning("El objeto 'box' no está asignado en el inspector");
+        }
     }
 }
+
